e559fd318aaf02d3374feba2d0f412bd20a37564
ShamanShine <lazun2016@gmail.com>
2024-01-24 15:28:19 +0200

    add code task#1

---
+++ js/task-1.js
@@ -0,0 +1,15 @@
+// Сначала находим все элементы ul с ID# 'categories'
+const categoriesList = document.getElementById('categories');
+// затем все элементы li.item в списке ul#categories
+const categoryItems = categoriesList.querySelectorAll('li.item');
+// потом выводим количество категорий в консоль
+console.log(`Numbers of categories: ${categoryItems.length}`);
+
+// проходимся по каждому элементу li.item, находим заголовки h2,и ищем все элементы li в вкладках, потом результат в консоль - это текст и количество элементов
+categoryItems.forEach(category => {
+  const categoryTitle = category.querySelector('h2').textContent;
+  const subcategoryItems = category.querySelectorAll('ul li');
+  console.log(
+    `Category: ${categoryTitle},\nElements: ${subcategoryItems.length}`,
+  );
+});
